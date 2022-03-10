40f160a98140f3cc734233f1cc7d271e0a2b1380
Richard Shea <rshea@thecubagroup.com>
2022-03-10 23:03:59 +1300

    Working, albeit limited, 'todo' app

--- flforrt/api.py
+++ flforrt/api.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Resource, Api
 
+todos = {}
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'cors'}
 
-api.add_resource(HelloWorld, '/')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
