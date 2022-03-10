fc52e45a2767124f9d00705485b9e847a98121b5
Richard Shea <rshea@thecubagroup.com>
2022-03-10 22:59:15 +1300

    After adding CORS capability to the existing app

--- flforrt/api.py
+++ flforrt/api.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'cors'}
 
 api.add_resource(HelloWorld, '/')
 
